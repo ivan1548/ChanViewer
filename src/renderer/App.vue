3a12098951623af55fdb08327da80cff87122e66
ivan1548 <ivan1548@tutamail.com>
2019-02-24 13:49:43 -0800

    Add toolbar

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,33 +1,16 @@
 <template>
   <div id="app">
-    <nav class="main-nav">
-      <div class="nav-container">
-        <ul class="nav-links">
-          <li>
-            <router-link to="/settings">
-              <i class="icon-settings"></i>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/">
-              <i class="icon-home"></i>
-            </router-link>
-          </li>
-          <li>
-            <a @click="$router.go(-1)" href="#">
-              <i class="icon-arrow-left"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <toolbar></toolbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Toolbar from "./components/Toolbar";
+
 export default {
-  name: "chan-viewer"
+  name: "chan-viewer",
+  components: { Toolbar }
 };
 </script>
 
---
+++ src/renderer/components/Toolbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="main-nav">
+    <div class="nav-container">
+      <ul class="nav-links">
+        <li>
+          <router-link to="/settings">
+            <i class="icon-settings"></i>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/">
+            <i class="icon-home"></i>
+          </router-link>
+        </li>
+        <li>
+          <a @click="$router.go(-1)" href="#">
+            <i class="icon-arrow-left"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "toolbar"
+};
+</script>
