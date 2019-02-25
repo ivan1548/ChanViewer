e0e6c4af2a72df5337f21ac94a90d4f44155320d
ivan1548 <ivan1548@tutamail.com>
2019-02-24 17:35:54 -0800

    Add filter to landing page

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="row">
     <div class="col col-sm-12">
+      <div class="form-control">
+        <input v-model="filterInput" type="text" placeholder="Filter">
+      </div>
       <div class="thread-list">
-        <div v-for="b in boards" v-bind:key="b.id">
+        <div v-for="b in filtered" v-bind:key="b.id">
           <router-link :to="`/board/${b.id}/1`">
             {{b.name}}
             <span v-if="b.nsfw">(NSFW)</span>
@@ -14,7 +17,7 @@
 </template>
 
 <script>
-import { map } from "rambda";
+import { map, filter, includes } from "rambda";
 
 import { getBoards } from "../helper/cache";
 
@@ -25,7 +28,8 @@ export default {
   components: {},
   data() {
     return {
-      boards: []
+      boards: [],
+      filterInput: ""
     };
   },
   created() {
@@ -44,6 +48,14 @@ export default {
   computed: {
     api() {
       return this.$store.state.Api.data;
+    },
+    filtered() {
+      if (this.filterInput === "") return this.boards;
+
+      const f = this.filterInput;
+      return filter(b => {
+        return includes(f, b.name.toLowerCase()) || includes(f, b.id);
+      })(this.boards);
     }
   }
 };
